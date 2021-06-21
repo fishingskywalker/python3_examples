5be46e5b65a58484607ccd1a43bfb1b88911e03b
jeffreygrant717 <86252022+jeffreygrant717@users.noreply.github.com>
2021-06-21 12:46:43 -0400

    update1
    Just some simple Python scripts because I promised a co-worker I would GIT IT DUN!

---
+++ c5.py
@@ -0,0 +1,19 @@
+# Boolean value
+val1 = True
+print(val1)
+
+# Number to Boolean
+number = 10
+print(bool(number))
+
+nuber = -5
+print(bool(number))
+
+number = 0
+print(bool(number))
+
+# Boolean from comparison operator
+val1 = 6
+val2 = 3
+print(val1 < val2)
+
---
+++ c6.py
@@ -0,0 +1,9 @@
+# Assign a numeric value
+number = 70
+
+# Check the is more than 70 or not
+if (number >= 70):
+    print("You have passed")
+else:
+    print("You have not passed")
+
