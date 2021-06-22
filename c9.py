7d899dfb7ae5763d8ccdc4da10fb7842db97a88e
jeffreygrant717 <86252022+jeffreygrant717@users.noreply.github.com>
2021-06-22 12:54:31 -0400

    update3
    just another simple modification

---
+++ c9.py
@@ -0,0 +1,9 @@
+# Initialize counter
+counter = 1
+# Iterate the loop 5 times
+while counter < 6:
+    # Print the counter value
+    print ("The current value: %d" % counter)
+    # Increment the counter
+    counter = counter + 1
+
