323550b46b19a5b9f5bb184af2269a7e3fe3995e
jeffreygrant717 <86252022+jeffreygrant717@users.noreply.github.com>
2021-06-28 12:39:11 -0400

    update
    update

---
+++ reverse_counter.py
@@ -0,0 +1,14 @@
+# Python3 code to demonstrate
+# backward iteration
+# using reversed()
+
+# Initializing number from which
+# iteration begins
+N = 6
+
+# Using reversed() to perform the back iteration
+print ("The reversed numbers are  ", end = "")
+for num in reversed(range(N + 1)):
+    print (num, end = " ")
+
+
