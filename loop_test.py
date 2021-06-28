b5f7bb908c959d43a048c290fa0c5471f0149bba
jeffreygrant717 <86252022+jeffreygrant717@users.noreply.github.com>
2021-06-28 17:13:21 -0400

    update
    update

---
+++ loop_test.py
@@ -0,0 +1,11 @@
+# This is just a simple test counter to test my Python skills
+
+sample1 = 1
+
+while sample1 < 11:
+    print ("The value of sample1 is currently: %d " % sample1)
+    sample1 = sample1 + 1
+
+
+
+
