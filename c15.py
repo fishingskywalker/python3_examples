4cf5bf21f229d2e73c91405fa83fce478cd00daf
jeffreygrant717 <86252022+jeffreygrant717@users.noreply.github.com>
2021-06-30 06:24:56 -0400

    update
    update

--- c15.py
+++ c15.py
@@ -4,9 +4,9 @@ from datetime import date
 current_date = date.today()
 
 # Print the formatted date
-print("Today is :%d-%d-%d" % (current_date.day,current_date.month,current_date.year))
+print("Today is: %d-%d-%d" % (current_date.day,current_date.month,current_date.year))
 
 # Set the custom date
 custom_date = date(2020, 12, 16)
-print("The date is:", custom_date)
+print("The date is:",custom_date)
 
